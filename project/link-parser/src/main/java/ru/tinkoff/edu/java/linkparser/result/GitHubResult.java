116178ea27b309a8000f98c0e31a9be5c6b82c92
Sviatoslav <55537912+svytoq@users.noreply.github.com>
2023-03-14 22:46:45 +0300

    Delete project directory

--- project/link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/result/GitHubResult.java
+++
@@ -1,4 +0,0 @@
-package ru.tinkoff.edu.java.linkparser.result;
-
-public record GitHubResult(String username, String repository) implements Result {
-}
--- project/link-parser/src/main/java/ru/tinkoff/edu/java/linkparser/result/Result.java
+++
@@ -1,4 +0,0 @@
-package ru.tinkoff.edu.java.linkparser.result;
-
-public sealed interface Result permits GitHubResult, StackOverflowResult {
-}
